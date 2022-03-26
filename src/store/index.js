6863a4b0be25ebd3c1d5f39836c276abf8b2a7fd
Nurtilek Karabekov <nurtilex@gmail.com>
2022-03-26 23:12:18 +0600

    fixed apply call

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,14 @@ import rootReducer from './reducer';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 const sagaMiddleware = createSagaMiddleware();
-
+const devTools =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(sagaMiddleware), devTools)
 );
 sagaMiddleware.run(rootSaga);
 
